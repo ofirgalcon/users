97ad8aa0854e0b2fa1d249a6a1780f6bb2768167
tuxudo <tvxudo@gmail.com>
2020-04-30 13:28:20 -0400

    Rearrange columns

--- users_controller.php
+++ users_controller.php
@@ -87,7 +87,7 @@ class Users_controller extends Module_controller
     public function get_tab_data($serial_number = '')
     {
        jsonView(
-            Users_model::selectRaw('record_name, real_name, password_hint, generated_uuid, home_directory, primary_group_id, administrator, ssh_access, screenshare_access, unique_id, user_shell, last_login_timestamp, creation_time, password_last_set_time, failed_login_count, failed_login_timestamp, password_history_depth, linked_full_name, linked_timestamp, group_memership, meta_record_name, email_address, smb_group_rid, smb_home, smb_home_drive, smb_primary_group_sid, smb_sid, smb_script_path, smb_password_last_set, original_node_name, primary_nt_domain, copy_timestamp')
+            Users_model::selectRaw('record_name, real_name, unique_id, password_hint, generated_uuid, home_directory, primary_group_id, administrator, ssh_access, screenshare_access, user_shell, last_login_timestamp, creation_time, password_last_set_time, failed_login_count, failed_login_timestamp, password_history_depth, linked_full_name, linked_timestamp, group_memership, meta_record_name, email_address, smb_group_rid, smb_home, smb_home_drive, smb_primary_group_sid, smb_sid, smb_script_path, smb_password_last_set, original_node_name, primary_nt_domain, copy_timestamp')
                 ->where('users.serial_number', $serial_number)
                 ->filter()
                 ->get()
