43b68bf0a06e2eda931eac68f03e15c6c04e5913
Arjen van Bochoven <arjen@bochoven.net>
2020-05-01 08:09:03 +0200

    Use core formatters

--- js/users.js
+++ js/users.js
@@ -1,27 +1,7 @@
-var formatUsersTime = function(col, row){
-    
-    var cell = $('td:eq('+col+')', row)
-    var checkin = parseInt(cell.text());
-    if (checkin > 0){
-        var date = new Date(checkin * 1000);
-        cell.html('<span title="'+date+'">'+moment(date).fromNow()+'</span>');
-    } else {
-        cell.text("")
-    }
-}
-
-var formatUsersYesNo = function(col, row){
-    var cell = $('td:eq('+col+')', row),
-    value = cell.text()
-    value = value == '1' ? '<span class="label label-success">'+i18n.t('yes')+'</span>' :
-    (value === '0' ? '<span class="label label-danger">'+i18n.t('no')+'</span>' : '')
-    cell.html(value)
-}
-
 var formatUsersYesNoSSH = function(col, row){
     var cell = $('td:eq('+col+')', row),
-    value = cell.text()
-    value = value == '1' ? '<span class="label label-danger">'+i18n.t('yes')+'</span>' :
-    (value === '0' ? '<span class="label label-success">'+i18n.t('no')+'</span>' : '')
+        value = cell.text()
+    value = value == '1' ? mr.label(i18n.t('yes'), 'danger') :
+        (value === '0' ? mr.label(i18n.t('no'), 'success') : '')
     cell.html(value)
 }
