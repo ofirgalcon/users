c09651c3b9e99dbe4b3d6e57e6dd8c9c1ce30743
tuxudo <tuxudo@users.noreply.github.com>
2020-05-29 08:42:40 -0400

    Fix missing current user data

--- views/users_detail_widget.php
+++ views/users_detail_widget.php
@@ -3,10 +3,10 @@
     <table id="mr-users-table">
         <tr>
             <th data-i18n="last_user"></th>
-            <td><span class="mr-long_username"></span> (<span class="mr-console_user"></span>)</td>
+            <td><span class="reportdata-long_username"></span> (<span class="reportdata-console_user"></span>)</td>
         </tr>
         <tr>
-            <th data-i18n="user_sessions.last_user_uid"></th><td><span class="mr-uid"></span></td>
+            <th data-i18n="user_sessions.last_user_uid"></th><td><span class="reportdata-uid"></span></td>
         </tr>
     </table>
 </div>
